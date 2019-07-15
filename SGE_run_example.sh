4c8ef8762d256435271db614283ffb7de22016c9
tschaefers <t.sch@jpberlin.de>
2019-07-15 11:10:39 +0200

    Update SGE_run_example.sh

--- SGE_run_example.sh
+++ SGE_run_example.sh
@@ -6,17 +6,27 @@
 #$ -V
 #$ -cwd
 #$ -l h_vmem=30G
-#$ -M t.schafers@umcutrecht.nl
+#$ -M your.mail@your.institution
 #$ -m beas
 
+VIRUSSEQ=/hpc/cog_bioinf/common_scripts/VirusSeq-CLI/sub/virusseq.sh
+
+#INPUTS
 FQ1=/hpc/cog_bioinf/ubec/analyses/custom/HUB_virusseq/fastq/L526401A_1.fq.gz
 FQ2=/hpc/cog_bioinf/ubec/analyses/custom/HUB_virusseq/fastq/L526401A_2.fq.gz
 OUT=/hpc/cog_bioinf/ubec/analyses/custom/HUB_virusseq/test2
 
-#Example. PE/SE without skipping hg19 alignment
-#sh sub/virusseq.sh -f $FQ1 -r $FQ2 -n 14 -o $OUT 
-#sh sub/virusseq.sh -f $FQ1 -n 14 -o $OUT 
 
-#Example. PE/SE with skipping hg19 alignment
-#sh sub/virusseq.sh -f $FQ1 -r $FQ2 -o $OUT -n 14 -s 
-#sh sub/virusseq.sh -f $FQ1 -o $OUT -n 14 -s 
+
+
+#### EXAMPLES ###### 
+
+#PE/SE without skipping hg19 alignment
+
+#sh ${VIRUSSEQ} -f $FQ1 -r $FQ2 -n 14 -o $OUT 
+#sh ${VIRUSSEQ} -f $FQ1 -n 14 -o $OUT 
+
+#PE/SE with skipping hg19 alignment
+
+#sh ${VIRUSSEQ} -f $FQ1 -r $FQ2 -o $OUT -n 14 -s 
+#sh ${VIRUSSEQ} -f $FQ1 -o $OUT -n 14 -s 
